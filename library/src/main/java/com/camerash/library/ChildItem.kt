cc55f6b2f9facc29a26cb6bc8f5ee034d34dab0e
camerash <esmond9981127@gmail.com>
2018-09-26 23:40:44 +0800

    Added base childItem

--- library/src/main/java/com/camerash/library/ChildItem.kt
+++ library/src/main/java/com/camerash/library/ChildItem.kt
@@ -12,6 +12,9 @@ abstract class ChildItem {
     abstract fun getViewHolder(v: View): ViewHolder
 
     abstract inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-
+        abstract fun bindView(child: ChildItem)
+        abstract fun onSelect(child: ChildItem)
+        abstract fun onDeselect(child: ChildItem)
+        abstract fun onReset(child: ChildItem)
     }
 }
