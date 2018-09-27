9c54716338fec21bbf5f4e92ae202cbe1bd3f924
camerash <esmond9981127@gmail.com>
2018-09-27 11:17:05 +0800

    Implement default child item

--- library/src/main/java/com/camerash/library/ChildItem.kt
+++ library/src/main/java/com/camerash/library/ChildItem.kt
@@ -2,6 +2,7 @@ package com.camerash.library
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.ViewGroup
 
 abstract class ChildItem {
 
@@ -9,9 +10,19 @@ abstract class ChildItem {
 
     abstract fun getLayoutRes(): Int
 
+    abstract fun getRootViewId(): Int
+
     abstract fun getViewHolder(v: View): ViewHolder
 
     abstract inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+        val rootView: ViewGroup = itemView.findViewById(getRootViewId())
+
+        internal fun bindView(child: ChildItem, onClickListener: (View) -> Unit) {
+            bindView(child)
+            rootView.setOnClickListener(onClickListener)
+        }
+
         abstract fun bindView(child: ChildItem)
         abstract fun onSelect(child: ChildItem)
         abstract fun onDeselect(child: ChildItem)
---
+++ library/src/main/java/com/camerash/library/DefaultChildItem.kt
@@ -0,0 +1,37 @@
+package com.camerash.library
+
+import android.graphics.Color
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.TextView
+
+abstract class DefaultChildItem: ChildItem() {
+
+    override fun getLayoutRes(): Int = R.layout.default_filter_child
+
+    override fun getViewHolder(v: View): ViewHolder = ViewHolder(v)
+
+    inner class ViewHolder(v: View): ChildItem.ViewHolder(v) {
+
+        private val childText: TextView = itemView.findViewById(R.id.child_text)
+
+        override fun bindView(child: ChildItem) {
+            childText.text = child.getTitle()
+            childText.setTextColor(Color.BLACK)
+        }
+
+        override fun onSelect(child: ChildItem) {
+            childText.setTextColor(ContextCompat.getColor(itemView.context, R.color.colorPrimary))
+            rootView.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.selected_gray))
+        }
+
+        override fun onDeselect(child: ChildItem) {
+            childText.setTextColor(Color.BLACK)
+            rootView.setBackgroundColor(Color.WHITE)
+        }
+
+        override fun onReset(child: ChildItem) {
+            onDeselect(child)
+        }
+    }
+}
