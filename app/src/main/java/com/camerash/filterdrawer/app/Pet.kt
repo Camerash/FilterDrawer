43d01d9cd44a33f550acdca9143c8354fca3f944
camerash <esmond9981127@gmail.com>
2018-10-03 16:11:28 +0800

    Adjust sample class data members

--- app/src/main/java/com/camerash/filterdrawer/app/Pet.kt
+++ app/src/main/java/com/camerash/filterdrawer/app/Pet.kt
@@ -1,4 +1,3 @@
 package com.camerash.filterdrawer.app
 
-class Pet {
-}
+class Pet(val name: String, val imageUrl: String, val kind: PetFilter.Kind, val size: PetFilter.Size)
--- app/src/main/java/com/camerash/filterdrawer/app/PetFilter.kt
+++ app/src/main/java/com/camerash/filterdrawer/app/PetFilter.kt
@@ -3,13 +3,12 @@ package com.camerash.filterdrawer.app
 import com.camerash.filterdrawer.DefaultChildItem
 import com.camerash.filterdrawer.R
 
-class PetFilter(name: String) : DefaultChildItem() {
+class PetFilter(val filter: Enum<*>) : DefaultChildItem() {
 
-    var name = ""
+    enum class Kind { Cats, Dogs, Rabbits, Hamsters, Snakes }
+    enum class Size { Small, Medium, Large }
 
-    init { this.name = name }
-
-    override fun getTitle(): String = this.name
+    override fun getTitle(): String = this.filter.name
 
     override fun getSelectedColorRes(): Int = R.color.colorPrimary
 }
