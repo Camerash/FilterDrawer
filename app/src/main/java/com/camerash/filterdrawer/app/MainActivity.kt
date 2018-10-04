9dfc82c94a1ed64b865395f521845363970be969
camerash <esmond9981127@gmail.com>
2018-10-04 15:01:23 +0800

    Finish sample app

--- app/src/main/java/com/camerash/filterdrawer/app/MainActivity.kt
+++ app/src/main/java/com/camerash/filterdrawer/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.camerash.filterdrawer.app
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.camerash.filterdrawer.DrawerBuilder
 import com.camerash.filterdrawer.FilterDrawer
@@ -15,17 +16,55 @@ class MainActivity : AppCompatActivity(), FilterDrawer.OnFilterControlClickListe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setSupportActionBar(toolbar)
 
-        val sampleItemList = constructSample()
+        initFilter()
+        initRecyclerView()
+    }
+
+    private fun initFilter() {
+        val petFilterList = constructFilterItems()
 
         filterDrawer = DrawerBuilder<PetFilterCategory, PetFilter>(this)
                 .displayToolbar(true)
                 .setFilterControlClickListener(this)
                 .addChildSelectListener(this)
-                .withItems(sampleItemList)
+                .withItems(petFilterList)
                 .build()
+    }
+
+    private fun initRecyclerView() {
+        val adapter = PetRecyclerAdapter(constructPetItems())
+        adapter.bindFilterDrawer(filterDrawer)
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.adapter = adapter
+    }
+
+    private fun constructFilterItems(): ArrayList<PetFilterCategory> {
+        val animal1 = PetFilter(PetFilter.Kind.Cats)
+        val animal2 = PetFilter(PetFilter.Kind.Dogs)
+        val animal3 = PetFilter(PetFilter.Kind.Rabbits)
+        val animal4 = PetFilter(PetFilter.Kind.Hamsters)
+        val animal5 = PetFilter(PetFilter.Kind.Snakes)
+
+        val pet = PetFilterCategory(PetFilterCategory.FilterType.Kind, R.drawable.round_pets_24, arrayListOf(animal1, animal2, animal3, animal4, animal5))
+
+        val size1 = PetFilter(PetFilter.Size.Small)
+        val size2 = PetFilter(PetFilter.Size.Medium)
+        val size3 = PetFilter(PetFilter.Size.Large)
+
+        val animal = PetFilterCategory(PetFilterCategory.FilterType.Size, R.drawable.round_size_24, arrayListOf(size1, size2, size3))
+
+        return arrayListOf(pet, animal)
+    }
+
+    private fun constructPetItems(): ArrayList<Pet> {
+        return arrayListOf(
+                Pet("Pembroke Welsh Corgi", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/WelshCorgi.jpeg/482px-WelshCorgi.jpeg", PetFilter.Kind.Dogs, PetFilter.Size.Medium),
+                Pet("British Shorthair Cat", "", PetFilter.Kind.Cats, PetFilter.Size.Medium),
+                Pet("Netherland Dwarf Rabbit", "", PetFilter.Kind.Rabbits, PetFilter.Size.Small)
+        )
     }
 
     override fun onResetClick() {
@@ -46,23 +85,5 @@ class MainActivity : AppCompatActivity(), FilterDrawer.OnFilterControlClickListe
 
     override fun onReset() {
         Log.d("reset", "true")
-    }
-
-    private fun constructSample(): ArrayList<PetFilterCategory> {
-        val animal1 = PetFilter(PetFilter.Kind.Cats)
-        val animal2 = PetFilter(PetFilter.Kind.Dogs)
-        val animal3 = PetFilter(PetFilter.Kind.Rabbits)
-        val animal4 = PetFilter(PetFilter.Kind.Hamsters)
-        val animal5 = PetFilter(PetFilter.Kind.Snakes)
-
-        val pet = PetFilterCategory(PetFilterCategory.FilterType.Kind, R.drawable.round_pets_24, arrayListOf(animal1, animal2, animal3, animal4, animal5))
-
-        val size1 = PetFilter(PetFilter.Size.Small)
-        val size2 = PetFilter(PetFilter.Size.Medium)
-        val size3 = PetFilter(PetFilter.Size.Large)
-
-        val animal = PetFilterCategory(PetFilterCategory.FilterType.Size, R.drawable.round_size_24, arrayListOf(size1, size2, size3))
-
-        return arrayListOf(pet, animal)
     }
 }
