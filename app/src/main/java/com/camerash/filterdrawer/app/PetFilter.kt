83e3804ed68d5f78f0975af85c67dc57c082b920
camerash <esmond9981127@gmail.com>
2018-10-09 15:19:10 +0800

    I love birds more than snakes <3

--- app/src/main/java/com/camerash/filterdrawer/app/PetFilter.kt
+++ app/src/main/java/com/camerash/filterdrawer/app/PetFilter.kt
@@ -5,7 +5,7 @@ import com.camerash.filterdrawer.R
 
 class PetFilter(val filter: Enum<*>) : DefaultChildItem() {
 
-    enum class Kind { Cats, Dogs, Rabbits, Hamsters, Snakes }
+    enum class Kind { Cats, Dogs, Rabbits, Hamsters, Birds }
     enum class Size { Small, Medium, Large }
 
     override fun getTitle(): String = this.filter.name
