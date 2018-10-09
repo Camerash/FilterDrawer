0854e685bb46d15f8079565451c8de3ff76306fc
camerash <esmond9981127@gmail.com>
2018-10-09 15:19:51 +0800

    Should have used map long time ago

--- app/src/main/java/com/camerash/filterdrawer/app/MainActivity.kt
+++ app/src/main/java/com/camerash/filterdrawer/app/MainActivity.kt
@@ -41,26 +41,17 @@ class MainActivity : AppCompatActivity(), FilterDrawer.OnFilterControlClickListe
     }
 
     private fun constructFilterItems(): ArrayList<PetFilterCategory> {
-        val animal1 = PetFilter(PetFilter.Kind.Cats)
-        val animal2 = PetFilter(PetFilter.Kind.Dogs)
-        val animal3 = PetFilter(PetFilter.Kind.Rabbits)
-        val animal4 = PetFilter(PetFilter.Kind.Hamsters)
-        val animal5 = PetFilter(PetFilter.Kind.Snakes)
 
-        val pet = PetFilterCategory(PetFilterCategory.FilterType.Kind, R.drawable.round_pets_24, arrayListOf(animal1, animal2, animal3, animal4, animal5))
+        val pet = PetFilterCategory(PetFilterCategory.FilterType.Kind, R.drawable.round_pets_24, PetFilter.Kind.values().map { PetFilter(it) })
 
-        val size1 = PetFilter(PetFilter.Size.Small)
-        val size2 = PetFilter(PetFilter.Size.Medium)
-        val size3 = PetFilter(PetFilter.Size.Large)
-
-        val animal = PetFilterCategory(PetFilterCategory.FilterType.Size, R.drawable.round_size_24, arrayListOf(size1, size2, size3))
+        val animal = PetFilterCategory(PetFilterCategory.FilterType.Size, R.drawable.round_size_24, PetFilter.Size.values().map { PetFilter(it) })
 
         return arrayListOf(pet, animal)
     }
 
     private fun constructPetItems(): ArrayList<Pet> {
         return arrayListOf(
-                Pet("Pembroke Welsh Corgi", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/WelshCorgi.jpeg/482px-WelshCorgi.jpeg", PetFilter.Kind.Dogs, PetFilter.Size.Medium),
+                Pet("Pembroke Welsh Corgi", "https://www.dogbreedslist.info/uploads/allimg/dog-pictures/Pembroke-Welsh-Corgi-2.jpg", PetFilter.Kind.Dogs, PetFilter.Size.Medium),
                 Pet("British Shorthair Cat", "https://cdn1-www.cattime.com/assets/uploads/gallery/british-shorthair-cats-and-kittens/british-shorthair-cats-and-kittens-5.jpg", PetFilter.Kind.Cats, PetFilter.Size.Medium),
                 Pet("Netherland Dwarf Rabbit", "https://cf.ltkcdn.net/small-pets/images/orig/204252-1697x1131-dwarf-rabbit_new.jpg", PetFilter.Kind.Rabbits, PetFilter.Size.Small)
         )
