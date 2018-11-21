66474601b9248861439839275446cb6585115c23
camerash <esmond9981127@gmail.com>
2018-11-21 17:27:20 +0800

    Add KDoc

--- library/src/main/java/com/camerash/filterdrawer/DiffItemCallback.kt
+++ library/src/main/java/com/camerash/filterdrawer/DiffItemCallback.kt
@@ -1,6 +1,28 @@
 package com.camerash.filterdrawer
 
+/**
+ * Interface used for adapting DiffCallback to custom data type
+ *
+ * @author Camerash
+ * @param Data the class adaping this interface
+ */
 interface DiffItemCallback <Data> {
+
+    /**
+     * Provides information on whether the two items are identical at a data level
+     * You should compare IDs or item-specific variables here
+     *
+     * @param item the other item
+     * @return whether the items are identical
+     */
     fun isIdentical(item: Data): Boolean
+
+    /**
+     * Provides information on whether the two items looks the same when shown to users
+     * You should compare resources shown to users here
+     *
+     * @param item the other item
+     * @return whether the items' presentations are the same
+     */
     fun hasSameRepresentation(item: Data): Boolean
 }
