54dae668acbfac73ee0289091002b9f209e9e5ee
camerash <esmond9981127@gmail.com>
2018-11-21 17:17:28 +0800

    Add KDoc

--- library/src/main/java/com/camerash/filterdrawer/ParentAdapter.kt
+++ library/src/main/java/com/camerash/filterdrawer/ParentAdapter.kt
@@ -4,20 +4,48 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 
+/**
+ * Adapter for ParentItem used in the FilterDrawer
+ *
+ * @author Camerash
+ * @param parentItemList list of ParentItem
+ * @param childSelectListenerList list of OnChildSelectListener to be called when changes happens
+ * @see ParentItem
+ * @see ChildAdapter
+ */
 class ParentAdapter<Parent, Child>(private var parentItemList: List<Parent>, var childSelectListenerList: ArrayList<FilterDrawer.OnChildSelectListener<Parent, Child>>)
     : RecyclerView.Adapter<RecyclerView.ViewHolder>() where Parent : ParentItem, Child : ChildItem {
 
+    /**
+     * The RecycledViewPool shared by all child RecyclerViews
+     */
     private val parentViewPool = RecyclerView.RecycledViewPool()
+
+    /**
+     * The list of ChildAdapter in FilterDrawer
+     */
     private val childAdapterList = arrayListOf<ChildAdapter<Parent, Child>>()
 
+    /**
+     * Inflate view and create view holder
+     */
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
             parentItemList.first().getViewHolder(LayoutInflater.from(viewGroup.context).inflate(R.layout.default_filter_parent, viewGroup, false))
 
+    /**
+     * @return Number of child filters
+     */
     override fun getItemCount(): Int = parentItemList.size
 
-    // Leave empty as we will be using the method that receives payloads
+    /**
+     * Called when view holder request binding
+     * Leave empty as we will be using the method that receives payloads
+     */
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {}
 
+    /**
+     * Called when view holder request binding
+     */
     @Suppress("UNCHECKED_CAST")
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, payloads: MutableList<Any>) {
         val parentItem = parentItemList[position]
@@ -51,10 +79,16 @@ class ParentAdapter<Parent, Child>(private var parentItemList: List<Parent>, var
         }
     }
 
+    /**
+     * Called when status of any ChildItem changes
+     */
     private fun handleChildSelect(adapterPosition: Int, childItem: Set<Child>, selected: Boolean) {
         notifyItemChanged(adapterPosition, Pair(childItem, selected))
     }
 
+    /**
+     * Get the map of ParentItem to set of currently selected ChildItem
+     */
     fun getSelectedChildren(): Map<Parent, Set<Child>> {
         val map = mutableMapOf<Parent, Set<Child>>()
 
@@ -66,11 +100,17 @@ class ParentAdapter<Parent, Child>(private var parentItemList: List<Parent>, var
         return map
     }
 
+    /**
+     * Update list of ParentItem
+     */
     fun updateItems(parentItemList: List<Parent>) {
         this.parentItemList = parentItemList
         notifyDataSetChanged()
     }
 
+    /**
+     * Reset FilterDrawer
+     */
     fun reset() {
         childAdapterList.forEach { it.reset() }
         notifyItemRangeChanged(0, childAdapterList.size, RESET_FLAG)
